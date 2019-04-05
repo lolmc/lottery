ec307e6360ae3c84d9acb2d903c8a21682f61506
Lol McBride <3874987+lolmc@users.noreply.github.com>
2019-04-05 13:38:06 +0100

    Add files via upload

---
+++ import_csv.py
@@ -0,0 +1,45 @@
+# Import `os`
+import os
+# Import module to deal with IP subnet calculations
+
+# Retrieve current working directory (`cwd`)
+cwd = os.getcwd()
+print(cwd)
+# Change directory
+os.chdir("/home/lol/PycharmProjects/lottery/")
+# List all files and directories in current directory
+print(os.listdir('.'))
+
+
+# Adventure with sqlite3
+
+# A minimal SQLite shell for experiments
+
+import sqlite3
+
+con = sqlite3.connect(":memory:")
+con.isolation_level = None
+cur = con.cursor()
+
+buffer = ""
+
+print("Enter your SQL commands to execute in sqlite3.")
+print("Enter a blank line to exit.")
+
+while True:
+    line = input()
+    if line == "":
+        break
+    buffer += line
+    if sqlite3.complete_statement(buffer):
+        try:
+            buffer = buffer.strip()
+            cur.execute(buffer)
+
+            if buffer.lstrip().upper().startswith("SELECT"):
+                print(cur.fetchall())
+        except sqlite3.Error as e:
+            print("An error occurred:", e.args[0])
+        buffer = ""
+
+con.close()
